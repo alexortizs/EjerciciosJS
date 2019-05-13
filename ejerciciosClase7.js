3b0ca396262ab65820f0bd7b7e3297cc68d22f2f
Edison Alexander Ortiz Santillan <edison.ortiz@caces.ec>
2019-05-13 09:17:40 -0500

    Registro inicial con el primer ejercicio de la clase 7

---
+++ ejerciciosClase7.js
@@ -0,0 +1,9 @@
+function calculosVarios(valor1, valor2){
+    console.info("El valor de la suma es:", valor1+valor2);
+    console.info("El valor de la resta es:",valor1-valor2);
+    console.info("El valor de la división es:", valor1/valor2);
+    console.info("El valor de la multiplicación es:", valor1*valor2)
+    console.info("El valor del residuo es:", valor1%valor2)
+};
+
+calculosVarios(11,5);
